b2229ac739777e67a039b5771e4dcc99fddf47da
CButtazoni <cbuttazoni@users.noreply.github.com>
2021-11-20 17:18:57 +0100

    nur für vorbereitung. wird nach der vl aktualisiert

---
+++ ChatAppStep3/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ChatBarComponent } from './components/chat-bar/chat-bar.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ChatBarComponent,
+  ],
+  imports: [BrowserModule, NgbModule, FormsModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
